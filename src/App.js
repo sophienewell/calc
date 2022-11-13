e30bb50029a2868444d4145b19469f5ff560d6d1
Sophie Newell <sophienewellstudio@gmail.com>
2022-11-13 14:35:42 -0600

    adds exponent function

--- src/App.js
+++ src/App.js
@@ -6,35 +6,59 @@ function App() {
   const [selectedFunc, setSelectedFunc] = useState(null);
   const [calcVal, setCalcVal] = useState("");
   const [solution, setSolution] = useState("");
+  const [pow, setPow] = useState(false);
 
   const clear = () => {
     setEnteredVal("");
     setCalcVal("");
     setSolution("");
+    setPow(false);
   };
 
   const addVal = (e) => {
-    setEnteredVal(enteredVal + e.target.innerText);
+    setEnteredVal(pow ? e.target.innerText : enteredVal + e.target.innerText);
     setCalcVal(calcVal + e.target.innerText);
+    setSelectedFunc("");
   };
 
   const plusMinus = (e) => {
     let symbol;
-    if (e.target.innerText === "x") {
-      symbol = "*";
+    if (pow) {
+      setPow(false);
+      symbol = ")";
     } else {
-      symbol = e.target.innerText;
+      symbol = "";
     }
+    if (e.target.innerText === "x") {
+      symbol = symbol + "*";
+    } else if (e.target.innerText === "(" && !selectedFunc) {
+      symbol = symbol + "*(";
+    } else {
+      symbol = symbol + e.target.innerText;
+    }
+
     setCalcVal(calcVal + symbol);
     setEnteredVal("");
     setSelectedFunc(symbol);
+  };
+
+  const exponent = () => {
+    setPow(true);
+    let lastVal = enteredVal.length;
+    console.log(lastVal);
+    setCalcVal(calcVal.slice(0, -lastVal) + "Math.pow(" + enteredVal + ", ");
   };
 
   const equals = () => {
     const calculate = (fn) => {
       return new Function("return " + fn)();
     };
-    setSolution(calculate(calcVal));
+    if (pow) {
+      setPow(false);
+      setSolution(calculate(calcVal + ")"));
+    } else {
+      setSolution(calculate(calcVal));
+    }
   };
 
   return (
@@ -43,9 +67,9 @@ function App() {
         <div className="display">{solution ? solution : enteredVal}</div>
         <div className="buttons">
           <button onClick={clear}>AC</button>
-          <button>(</button>
-          <button>)</button>
-          <button>xy</button>
+          <button onClick={plusMinus}>(</button>
+          <button onClick={plusMinus}>)</button>
+          <button onClick={exponent}>xy</button>
           <button onClick={addVal}>7</button>
           <button onClick={addVal}>8</button>
           <button onClick={addVal}>9</button>
@@ -62,6 +86,7 @@ function App() {
           <button onClick={addVal}>.</button>
           <button onClick={plusMinus}>+</button>
           <button onClick={equals}>=</button>
+          <button>{pow ? "T" : "F"}</button>
         </div>
       </div>
       <p>{calcVal}</p>
