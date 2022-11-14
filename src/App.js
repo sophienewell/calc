d401f923dd279085fe3046ef64ce98d8c816a7bf
Sophie Newell <sophienewellstudio@gmail.com>
2022-11-13 20:20:47 -0600

    fixes some errors with exponent calcs

--- src/App.js
+++ src/App.js
@@ -18,20 +18,21 @@ function App() {
   const addVal = (e) => {
     setEnteredVal(pow ? e.target.innerText : enteredVal + e.target.innerText);
     setCalcVal(calcVal + e.target.innerText);
-    setSelectedFunc("");
+    //setSelectedFunc("");
   };
 
   const plusMinus = (e) => {
     let symbol;
-    if (pow) {
+    if (pow && e.target.innerText !== "(" && selectedFunc !== "(") {
       setPow(false);
       symbol = ")";
     } else {
       symbol = "";
     }
+
     if (e.target.innerText === "x") {
       symbol = symbol + "*";
-    } else if (e.target.innerText === "(" && !selectedFunc) {
+    } else if (e.target.innerText === "(" && !selectedFunc && calcVal && !pow) {
       symbol = symbol + "*(";
     } else {
       symbol = symbol + e.target.innerText;
@@ -42,23 +43,43 @@ function App() {
     setSelectedFunc(symbol);
   };
 
+  const closeParenth = () => {
+    if (pow) {
+      setCalcVal(calcVal + ")");
+    } else {
+      let parenthLength = calcVal.lastIndexOf("(");
+      let preParenthContent = calcVal.slice(0, parenthLength);
+      let parenthVal = eval(calcVal.slice(parenthLength, calcVal.length) + ")");
+      setCalcVal(preParenthContent + parenthVal);
+      setEnteredVal(parenthVal);
+    }
+  };
+
   const exponent = () => {
     setPow(true);
-    let lastVal = enteredVal.length;
-    console.log(lastVal);
-    setCalcVal(calcVal.slice(0, -lastVal) + "Math.pow(" + enteredVal + ", ");
+    let lastValLength = enteredVal.length;
+    setCalcVal(
+      calcVal.toString().slice(0, -lastValLength) +
+        "Math.pow(" +
+        enteredVal +
+        ", "
+    );
   };
 
   const equals = () => {
     const calculate = (fn) => {
       return new Function("return " + fn)();
     };
+    let newVal;
     if (pow) {
       setPow(false);
-      setSolution(calculate(calcVal + ")"));
+      newVal = calculate(calcVal + ")");
     } else {
-      setSolution(calculate(calcVal));
+      newVal = calculate(calcVal);
     }
+    setSolution(newVal);
+    setCalcVal(newVal);
+    setEnteredVal(newVal);
   };
 
   return (
@@ -68,7 +89,7 @@ function App() {
         <div className="buttons">
           <button onClick={clear}>AC</button>
           <button onClick={plusMinus}>(</button>
-          <button onClick={plusMinus}>)</button>
+          <button onClick={closeParenth}>)</button>
           <button onClick={exponent}>xy</button>
           <button onClick={addVal}>7</button>
           <button onClick={addVal}>8</button>
@@ -86,10 +107,8 @@ function App() {
           <button onClick={addVal}>.</button>
           <button onClick={plusMinus}>+</button>
           <button onClick={equals}>=</button>
-          <button>{pow ? "T" : "F"}</button>
         </div>
       </div>
-      <p>{calcVal}</p>
     </>
   );
 }
